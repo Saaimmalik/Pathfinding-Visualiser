31849f31562c11c47d90e859fdd32041b0552e64
Saaim Malik <saaimmalik832@gmail.com>
2026-08-16 22:17:50 +0500

    making grid

---
+++ main.py
@@ -0,0 +1,96 @@
+import pygame 
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+class Node:
+    def __init__ (self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.colour = WHITE
+        self.neighbours = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+    def is_closed(self):
+        return self.colour == RED
+
+    def is_open(self):
+        return self.colour == GREEN
+
+    def is_barrier(self):
+        return self.colour == BLACK
+
+    def is_start(self):
+        return self.colour == ORANGE
+
+    def is_end(self):
+        return self.colour == TURQUOISE
+
+    def reset(self):
+        self.colour == WHITE
+
+    def make_closed(self):
+        self.colour = RED
+
+    def make_open(self):
+        self.colour = GREEN
+
+    def make_barrier(self):
+        self.colour = BLACK
+
+    def make_end(self):
+        self.colour = TURQUOISE
+
+    def make_path(self):
+        self.colour = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.width))
+
+    def update_neighbour(self, grid):
+        pass
+
+    def __it__(self, other):
+        return False   
+
+
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1-y2)
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
+
+    return grid
+
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i*gap), (width, i * gap))
